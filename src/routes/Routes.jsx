bdc5b54e6d1084b90eb418813162a24e5c8b0d3e
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2024-12-23 23:44:40 +0600

    project initialized done

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div className="text-4xl font-bold">Group Study!</div>,
+    },
+]);
+
+export default router;
