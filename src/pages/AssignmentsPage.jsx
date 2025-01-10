2612b2c2986a3ae355f1c5a71d2053295afa9c7a
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-01-11 02:08:34 +0600

    assignments page modified

--- src/pages/AssignmentsPage.jsx
+++ src/pages/AssignmentsPage.jsx
@@ -147,7 +147,7 @@ const AssignmentsPage = () => {
             Reset
           </button>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-6">
           {assignments.map((assignment) => (
             <div
               key={assignment._id}
